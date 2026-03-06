6cd0c76e4909af9fdc880cbb9a80d5327a2446f0
Aléxia Andrade <alexiafernanda424@gmail.com>
2026-03-05 21:27:32 -0300

    Ex1 - Concluido

--- pratica1/src/Main.java
+++ pratica1/src/Main.java
@@ -7,9 +7,15 @@ public class Main {
         Scanner entrada = new Scanner(System.in);
         TrianguloRetangulo tr = new TrianguloRetangulo();
 
-        System.out.println("Digite o cateto 1 e o cateto 2?");
+        System.out.println("Digite o cateto 1 e o cateto 2: ");
         double c1 = entrada.nextDouble();
+        tr.setC1(c1);
         double c2 = entrada.nextDouble();
+        tr.setC2(c2);
 
+        double resultadoH = tr.calculaHipotenusa();
+        double resultadoA = tr.calculaArea();
+        System.out.println("O resultado da hipotenusa e: "+ resultadoH);
+        System.out.println("O resultado da area e: "+ resultadoA);
     }
 }
--- pratica1/src/TrianguloRetangulo.java
+++ pratica1/src/TrianguloRetangulo.java
@@ -7,31 +7,25 @@ public class TrianguloRetangulo{
     private double a;
 
     // Metodos
-
-    public void setC1(c1){
-        if (c1>0){
+    public void setC1(double c1){
             this.cateto1 = c1;
-        }
     }
+    public void setC2(double c2){
+        this.cateto2 = c2;
+    }
+
     public double getC1(){
         return cateto1;
-        }
-    }
-    public void setC2(c2){
-        if (c2>0){
-            this.cateto2 = c2;
-        }
     }
     public double getC2(){
         return cateto2;
     }
-
     public double calculaHipotenusa() {
-        h = Math.sqrt(Math.pow(c1, 2) + Math.pow(c2, 2));
+        h = Math.sqrt(Math.pow(getC1(), 2) + Math.pow(getC2(), 2));
         return h;
     }
     public double calculaArea() {
         // Num triângulo retângulo, os catetos são a base e a altura
-        return (c1 * c2) / 2;
+        return (getC1() * getC2()) / 2;
     }
 }
